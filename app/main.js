c7710c73879fd6c0720cef95a82eca6eb557ea8f
DominikKlyncypar <dominikklyncypar@gmail.com>
2025-08-12 15:35:34 -0600

    Initial Electron app

---
+++ app/main.js
@@ -0,0 +1,33 @@
+// 1. Import the app (controls app lifecycle) and BrowserWindow (creates windows)
+const { app, BrowserWindow } = require('electron')
+
+// 2. A function to create our window
+function createWindow() {
+  // Create a new BrowserWindow object with size
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  // Load a local HTML file into the window
+  win.loadFile('index.html')
+}
+
+// 3. Wait until Electron is ready, then create the window
+app.whenReady().then(() => {
+  createWindow()
+
+  // On macOS, re-create a window when clicking the dock icon if none are open
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// 4. Quit the app when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
